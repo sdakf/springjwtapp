8004ce41310d519dc1858b4bb50cb14a10e5292a
kf <falinskikrzysztof@gmail.com>
2020-10-07 12:29:23 +0200

    update

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,17 +6,15 @@ import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {VehiclesListComponent} from './vehicles-list/vehicles-list.component';
 
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatSidenavModule,
-  MatTabsModule,
-  MatToolbarModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {VehicleEditComponent} from './vehicle-edit/vehicle-edit.component';
 import {FormsModule} from '@angular/forms';
