0dc7f94f9b43392643dd82848f2413642af4253d
KF <k.falinski@sdacademy.pl>
2020-10-09 15:25:49 +0200

    a

--- springjwt/src/main/java/com/example/springjwt/DataInitializer.java
+++ springjwt/src/main/java/com/example/springjwt/DataInitializer.java
@@ -3,23 +3,24 @@ package com.example.springjwt;
 import com.example.springjwt.domain.User;
 import com.example.springjwt.domain.Vehicle;
 import com.google.common.collect.Lists;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Component
-@Slf4j
 public class DataInitializer extends AppDataInitializer {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(DataInitializer.class);
 
     @Override
     protected void initializeDataInDb() {
         if (vehicleRepository.count() == 0) {
-//            log.info("initializing vehicles data...");
+            log.info("initializing vehicles data...");
             Arrays.asList("BMW", "AUDI").forEach(v -> this.vehicleRepository.saveAndFlush(Vehicle.builder().model(v).build()));
         }
-//        log.info("printing all vehicles...");
-//        vehicleRepository.findAll().forEach(v -> log.info(" Vehicle :" + v.toString()));
+        log.info("printing all vehicles...");
+        vehicleRepository.findAll().forEach(v -> log.info(" Vehicle :" + v.toString()));
         if (userRepository.count() == 0) {
             userRepository.saveAndFlush(User.builder().username("apiUser").password(passwordEncoder.encode("apiUserPass")).roles(Lists.newArrayList("ROLE_USER")).build());
             userRepository.saveAndFlush(User.builder().username("apiAdmin").password(passwordEncoder.encode("apiAdminPass")).roles(Lists.newArrayList("ROLE_ADMIN")).build());
